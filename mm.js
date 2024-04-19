fbffab974871e10e5d63d78b7e7afe0f886ba0ec
Sariahmdsad <167486662+Sariahmdsad@users.noreply.github.com>
2024-04-19 22:22:13 +0300

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,18 @@
+let ff =document.getElementById("ff")
+let vv =document.getElementById("vv")
+let zzz =document.getElementById("zzz")
+let hhh =document.getElementById("hhh")
+function ccc() {
+  ff.play("")
+let yyy =document.getElementById("yyy").value
+vv.innerText =yyy;
+let hgf =Math.floor(Math.random() * 2) +1;
+console.log(hgf)
+if (hgf==1) {
+  zzz.style.background ="green";
+} else {
+  zzz.style.background ="red";
+}
+
+}
+
